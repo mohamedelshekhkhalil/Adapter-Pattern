d2da9d3bb80c1099b4ecc91c82cb690648575e79
Mohamed Elshekh Khalil <40995023+mohamedelshekhkhalil@users.noreply.github.com>
2023-02-04 15:10:21 +0300

    Add files via upload

---
+++ AdapterPattern/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class Adapter : ITarget
+    {
+        public Adaptee _adaptee { get; set; }
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public string GetTargetRequest()
+        {
+            return $"This is '{this._adaptee.GetAdapteeRequest()}'";
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdapterPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Adaptee ad = new Adaptee();
+        ITarget target = new Adapter(ad);
+        Console.WriteLine(target.GetTargetRequest());
+    }
+}
